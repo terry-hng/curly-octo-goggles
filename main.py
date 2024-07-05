7aa4e830459a0d3d30c789e99061212d64ce99f8
Terry Nguyen <40750175+terry-hng@users.noreply.github.com>
2024-07-05 17:58:52 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+from investpy import news
+import requests
+import datetime as dt
+import pytz
+
+def there_is_news_incoming(news_list):
+    for news in news_list:
+        if dt.datetime.strptime(news["time"], time_format) - dt.datetime.strptime(dt.datetime.now().strftime(time_format), time_format) <= dt.timedelta(hours=1):
+            return True
+
+
+discord_channel_url = "https://discord.com/api/v9/channels/1258699001268666390/messages"
+headers = {
+    "Authorization": os.environ.get("DISCORD_AUTH_KEY")
+}  # auth key needed to send messages through discord
+
+df = news.economic_calendar(importances=["high"], countries=["united states", "new zealand", "australia", "euro zone", "united kingdom", 'china'])
+
+news_list = df[["time", "zone", "event"]].to_dict(orient="records")
+
+flags_emoji_dict = {
+    "new zealand": "🇳🇿",
+    "australia": "🇦🇺",
+    "united states": "🇺🇸",
+    "china": "🇨🇳",
+    "euro zone": "🇪🇺",
+    "united kingdom": "🇬🇧"
+}
+
+time_format = '%H:%M'
+
+if there_is_news_incoming(news_list=news_list):
+    message = "> **Incoming News❗❗❗**\n\n"
+
+    for news in news_list:
+        if dt.datetime.strptime(news["time"], time_format) - dt.datetime.strptime(dt.datetime.now().strftime(time_format), time_format) <= dt.timedelta(hours=1):
+            
+            message += f"{dt.datetime.strptime(news["time"], "%H:%M").replace(tzinfo=pytz.utc).astimezone(dt.timezone(dt.timedelta(hours=7))).strftime("%H:%M")}\t|\t{news["zone"].title()}    {flags_emoji_dict[news["zone"]]}\t|\t**{news["event"]}**\n\n"
+
+    payload = {"content": message + "---------------------------------\n"}
+
+    requests.post(discord_channel_url, payload, headers=headers)
